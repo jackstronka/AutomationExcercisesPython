9b94361961c46f9603feaa58224b673169c07665
Jacek Stronka <jackstronka@yahoo.com>
2026-02-13 09:38:26 +0100

    CI: use ubuntu-22.04, explicit CI env, more Chrome flags, --tb=long

--- utilities/web_driver_factory.py
+++ utilities/web_driver_factory.py
@@ -39,6 +39,8 @@ def create():
             options.add_argument("--no-sandbox")
             options.add_argument("--disable-dev-shm-usage")
             options.add_argument("--disable-gpu")
+            options.add_argument("--disable-software-rasterizer")
+            options.add_argument("--disable-extensions")
         service = ChromeService(ChromeDriverManager().install())
         driver = webdriver.Chrome(service=service, options=options)
     elif browser == "firefox":
